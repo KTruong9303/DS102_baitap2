431545b7264e65106d5068c147c2b7699ebff2cd
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-11-29 11:04:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class_list = {'0' : 'NORMAL', '1' : 'Pneumonia'}
 
 st.title('Pneu Detection')
 
-with open('lrc_xray.pkl', 'rb') as file:
-  model = pkl.load(file)
+input = open('lrc_xray.pkl', 'rb')
+model = pkl.load(input)
 
 st.header('up image')
 image = st.file_uploader('choose!', type = (['png', 'jpg', 'jpeg']))
